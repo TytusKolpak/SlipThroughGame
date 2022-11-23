53a25a2c8e5687787199b63fe51d9307b5ecc3bf
TytusKolpak <101522301+TytusKolpak@users.noreply.github.com>
2022-11-23 15:15:13 +0100

    Fixed the .gitignore file

--- Form1.cs
+++ Form1.cs
@@ -302,7 +302,7 @@ namespace Slip_through
                 //FIGHT
                 flowLayoutLongLog.Visible = false;
 
-                combatText = "";
+                combatText = "COMBAT LOG \n";
                 combatText += $"{enemy.name} attacks {player.name}\n";
                 combatText += $"{player.name} ({player.attack},{player.defence},{player.effectiveness},{player.hitPoints})\n";
                 combatText += $"{enemy.name} ({enemy.attack},{enemy.defence},{enemy.effectiveness},{enemy.hitPoints})\n\n";
